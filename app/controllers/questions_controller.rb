42ea170394c604dc5d3f363f06d037d4c29bcc1f
Julien Bidart <jbidart@ncsu.edu>
2020-05-11 16:30:33 +0200

    should be working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,15 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @answer = params['answer']
+    if @answer == 'I am going to work'
+      @message = 'Great!'
+    elsif @answer[-1, 1] == '?'
+      @message = 'Silly question, get dressed and go to work!'
+    else
+      @message = "I don't care, get dressed and go to work"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'answers/answer'
+  get 'answer', to: 'questions#answer'
   get 'ask', to: 'questions#ask'
 end
